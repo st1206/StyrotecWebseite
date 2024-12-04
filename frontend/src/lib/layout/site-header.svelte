9e337ec319163801b58994cfb3b6ebba7d690264
daniloleyalves <danilo.leyalves@gmail.com>
2024-12-04 16:42:29 +0100

    initial commit

---
+++ frontend/src/lib/layout/site-header.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { Navbar, NavBrand, NavHamburger, NavUl, NavLi, MegaMenu } from 'flowbite-svelte';
+	import { ChevronDownOutline } from 'flowbite-svelte-icons';
+
+	let menu = [
+		{ name: 'Online Stores', help: "Connect with third-party tools that you're already using." },
+		{ name: 'Segmentation', help: "Connect with third-party tools that you're already using." },
+		{ name: 'Marketing CRM', help: "Connect with third-party tools that you're already using." },
+
+		{ name: 'Online Stores', help: "Connect with third-party tools that you're already using." },
+		{ name: 'Segmentation', help: "Connect with third-party tools that you're already using." },
+		{ name: 'Marketing CRM', help: "Connect with third-party tools that you're already using." },
+
+		{
+			name: 'Audience Management',
+			help: "Connect with third-party tools that you're already using."
+		},
+		{ name: 'Creative Tools', help: "Connect with third-party tools that you're already using." },
+		{
+			name: 'Marketing Automation',
+			help: "Connect with third-party tools that you're already using."
+		}
+	];
+</script>
+
+<Navbar let:hidden let:toggle class="z-50">
+	<NavBrand href="/">
+		<img
+			src="https://flowbite-svelte.com/images/flowbite-svelte-icon-logo.svg"
+			class="me-3 h-6 sm:h-9"
+			alt="Flowbite Logo"
+		/>
+		<span class="self-center whitespace-nowrap text-xl font-semibold">Flowbite</span>
+	</NavBrand>
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden}>
+		<NavLi href="/">Home</NavLi>
+		<NavLi class="cursor-pointer">
+			Mega menu<ChevronDownOutline class="ms-2 inline h-6 w-6 text-primary-800 " />
+		</NavLi>
+		<MegaMenu full items={menu} let:item class="bg-white">
+			<a href="/" class="block h-full rounded-lg p-3 hover:bg-gray-50">
+				<div class="font-semibold">{item.name}</div>
+				<span class="text-sm font-light text-gray-500">{item.help}</span>
+			</a>
+		</MegaMenu>
+		<NavLi href="/services">Services</NavLi>
+		<NavLi href="/services">Products</NavLi>
+		<NavLi href="/services">Contact</NavLi>
+	</NavUl>
+</Navbar>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import SiteHeader from '$lib/layout/site-header.svelte';
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<div class="relative flex min-h-screen flex-col items-center">
+	<SiteHeader />
+	<main class="flex-1 sm:container">
+		{@render children()}
+	</main>
+</div>
+
+<svelte:head>
+	<!-- <title></title>
+	<meta name="description" content="" />
+	<meta name="keywords" content={} />
+	<meta name="author" content="" />
+	<meta property="og:title" content="" />
+	<meta property="og:url" content={} />
+	<meta property="og:image" content="" />
+	<meta property="og:image:alt" content={} />
+	<meta property="og:description" content={} />
+	<meta property="og:site_name" content={} />
+	<meta property="og:locale" content="DE_DE" />
+	<link rel="shortcut icon" href="" /> -->
+</svelte:head>
