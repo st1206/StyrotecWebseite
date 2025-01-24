a5c3f50a5bd85c9d902fd5f5c35de6928c2cba02
Danilo Ley Alves <danilo.leyalves@gmail.com>
2025-01-24 14:51:47 +0100

    backend refactor

--- backend/src/api/bilder/controllers/bilder.ts
+++
@@ -1,7 +0,0 @@
-/**
- * bilder controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::bilder.bilder');
--- backend/src/api/bilder/routes/bilder.ts
+++
@@ -1,7 +0,0 @@
-/**
- * bilder router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::bilder.bilder');
