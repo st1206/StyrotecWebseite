63207e3d9f0ccd061b3ba0bff13674f3edaebbe6
stephanSchuster <stephanauswaldburg@web.de>
2025-01-30 08:01:14 +0100

    test3

--- frontend/src/routes/+error.svelte
+++ frontend/src/routes/+error.svelte
@@ -2,5 +2,5 @@
 	class="mt-[200px] flex h-full w-full flex-col items-center justify-center gap-4 px-4 text-center"
 >
 	<h1 class="text-7xl font-semibold">404</h1>
-	<p class="text-lg font-medium">Seite nicht gefunden</p>
+	<p class="text-lg font-medium">Seite nicht gefunden !!!</p>
 </div>
