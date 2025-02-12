a83d4ecb18e552c3691689ab355d7b0395888ceb
stephanSchuster <stephanauswaldburg@web.de>
2025-02-12 08:57:40 +0100

    api token name angepasst

--- frontend/src/lib/server/utils.ts
+++ frontend/src/lib/server/utils.ts
@@ -1,8 +1,8 @@
-import { BACKEND_API_TOKEN } from '$env/static/private';
+import { PRIVATE_BACKEND_API_TOKEN } from '$env/static/private';
 
 export function getRequestHeaders(): Record<string, string> {
 	return {
 		'Content-Type': 'application/json',
-		Authorization: `Bearer ${BACKEND_API_TOKEN}`
+		Authorization: `Bearer ${PRIVATE_BACKEND_API_TOKEN}`
 	};
 }
