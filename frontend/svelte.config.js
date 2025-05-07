d3a44b2f6e31111ef37bbc6deb4814234464d752
Danilo Ley Alves <danilo.leyalves@gmail.com>
2025-05-08 00:33:13 +0200

    wip

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,13 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(),
 		prerender: {
 			entries: ['*']
 		}
